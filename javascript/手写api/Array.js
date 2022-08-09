af03829569a104d1bebc3d9951dfdcd3d9b39cdd
Rongrubujing <705456001@qq.com>
2022-08-09 22:54:15 +0800

    array api every/some/reduce/findindex

--- javascript/手写api/Array.js
+++ javascript/手写api/Array.js
@@ -14,7 +14,7 @@ Array.prototype.myforEach = function (callback) {
 }
 
 
-players.sx_forEach((item, index, arr) => {
+players.myforEach((item, index, arr) => {
   console.log(item, index)
 })
 // { name: '科比', num: 24 } 0
@@ -32,9 +32,10 @@ Array.prototype.myMap = function (callback) {
   for (let i = 0; i < this.length; i++) {
     res.push(callback(this[i], i, this))
   }
+  return res
 }
 
-console.log(players.sx_map((item, index) => `${item.name}--${item.num}--${index}`))
+console.log(players.myMap((item, index) => `${item.name}--${item.num}--${index}`))
 // [ '科比--24--0', '詹姆斯--23--1', '保罗--3--2', '威少--0--3', '杜兰特--35--4' ]
 
 
@@ -46,11 +47,64 @@ Array.prototype.myFilter = function(callback) {
   for(let i = 0; i < this.length; i++) {
     callback(this[i], i, this) && res.push(this[i])
   }
+  return res
 }
 
-console.log(players.sx_filter(item => item.num >= 23))
+console.log(players.myFilter(item => item.num >= 23))
 // [
 //     { name: '科比', num: 24 },
 //     { name: '詹姆斯', num: 23 },
 //     { name: '杜兰特', num: 35 }
 // ]
+
+// every
+Array.prototype.myEvery = function(callback) {
+  let flag = true
+
+  for(let i = 0; i < this.length; i++) {
+    flag = callback(this[i], i, this)
+    if(!flag) break
+  }
+
+  return flag
+}
+
+console.log(players.myEvery(item => item.num >= 23))
+
+// some
+Array.prototype.mySome = function(callback) {
+  let flag = false
+
+  for(let i = 0; i < this.length; i++) {
+    flag = callback(this[i], i, this)
+    if(flag) break;
+  }
+
+  return flag
+}
+
+
+console.log(players.mySome(item => item.num >= 23)) // true
+console.log(players.mySome(item => item.num >= 50)) // false
+
+// reduce
+
+Array.prototype.myReduce = function(callback, ...arg) {
+  let start = 0, total;
+  if(arg.length) {
+    total = arg[0]
+  }else {
+    pre = this[0];
+    start = 1;
+  }
+
+  for(let i = start; i < this.length; i++) {
+    total = callback(total, this[i], i, this)
+  }
+  return total
+}
+
+const sum = players.myReduce((total, next) => {
+  return total + next.num
+}, 0)
+console.log(sum) // 85
