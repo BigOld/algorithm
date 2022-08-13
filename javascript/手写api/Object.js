43eae1ae48a1042da210ad93258d56d7908cd4f3
Rongrubujing <705456001@qq.com>
2022-08-13 23:04:33 +0800

    object api

--- javascript/手写api/Object.js
+++ javascript/手写api/Object.js
@@ -54,3 +54,75 @@ Object.prototype.my_values = function(obj) {
 }
 
 console.log(Object.my_values(obj))
+
+// instanceof
+// 判断parent 是否在 child 的原型链上
+
+function instanceOf(father, child) {
+  let fp = father.prototype
+  let cp = child.__proto__
+
+  while(cp) {
+    if(cp === fp)
+      return true
+    cp = cp.__proto__
+  }
+  return false
+}
+
+// test
+function Person(name) {
+  this.name = name
+}
+
+let mason = new Person('mason')
+let jack = {
+  name: 'jack'
+}
+
+console.log(instanceOf(Person, mason)) // true
+console.log(instanceOf(Person, jack)) // false
+
+
+// is
+
+Object.prototype.my_is = function(x, y) {
+  if(x === y) {
+    return x !== 0 || 1 / x === 1 / y // 防止 -0 和 +0
+  }
+  // 一个变量不等于自身变量,那么它一定是 NaN
+  // 两个都是NaN的时候返回true
+  return x !== x && y !== y
+}
+
+const a = { name: 'mason' }
+const b = a
+const c = { name: 'mason' }
+console.log(Object.my_is(a, b)) // true
+console.log(Object.my_is(a, c)) // false
+console.log(Object.my_is(NaN, NaN)) // true
+console.log(Object.my_is(-0, +0)) // false
+
+// assign
+
+Object.prototype.my_assign = function(target, ...args) {
+  if(target === null || target === undefined) {
+    throw new TypeError('Cannot convert undefined or null to object')
+  }
+
+  target = Object(target) // 防止传进来不是对象
+
+  for(let arg of args) {
+    for(let key in arg) {
+      arg.hasOwnProperty(key) && (target[key] = arg[key])
+    }
+  }
+  return target
+}
+const testa = { name: 'mason' }
+const testb = { name: 'jack', age: 22 }
+const testc = { age: 18, gender: '男' }
+
+const testd = Object.my_assign(testa, testb, testc)
+console.log(testd) // { name: 'jack', age: 18, gender: '男' }
+console.log(testa === testd) // true
